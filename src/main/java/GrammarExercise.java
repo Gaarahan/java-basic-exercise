4359f48d4e6d4cf16f8841372fbc6008568462c1
gaarahan <gaarahan@foxmail.com>
2020-07-27 22:45:03 +0800

    [Gaarahan] GrammarExercise completed

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,43 @@
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * @author gaarahan
+ */
 public class GrammarExercise {
-    public static void main(String[] args) {
-        //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+  public static void main(String[] args) {
+    //需要从命令行读入
+    Scanner in = new Scanner(System.in);
+    String firstWordList = in.nextLine();
+    String secondWordList = in.nextLine();
 
-        List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
 
+    List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
+    //按要求输出到命令行
+    System.out.println(result);
+
+  }
+  public static List<String> findCommonWordsWithSpace(String firstWordStr, String secondWordStr) {
+    final String wordPattern = "^([a-zA-Z]+,)*[a-zA-Z]+$";
+    if (!Pattern.matches(wordPattern, firstWordStr) || !Pattern.matches(wordPattern, secondWordStr)) {
+      throw new RuntimeException("input not valid");
     }
 
-    public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
-    }
+    String[] firstWordArray = firstWordStr.split(",");
+    String[] secondWordArray = secondWordStr.split(",");
+
+    List<String> secondWordList = Stream.of(secondWordArray)
+        .map(String::toUpperCase)
+        .collect(Collectors.toList());
+    return Stream.of(firstWordArray)
+        .map(String::toUpperCase)
+        .filter(secondWordList::contains)
+        .distinct()
+        .sorted()
+        .map(itm -> itm.replace("", " ").trim())
+        .collect(Collectors.toList());
+  }
 }
